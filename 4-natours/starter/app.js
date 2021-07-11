99b77e0e80ff3505abf4067becab21eea163c9ce
Horacinis <horacinis@gmail.com>
2021-07-12 00:03:04 +1000

    ...

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -41,7 +41,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   const id = +req.params.id;
   const tour = tours.find(tour => tour.id === id);
 
-    // if (id > tours.length) {
+  // if (id > tours.length) {
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
@@ -76,6 +76,23 @@ app.post('/api/v1/tours', (req, res) => {
         }
       });
   });
+});
+
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    })
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  })
 });
 
 
