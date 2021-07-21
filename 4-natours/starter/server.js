78480bebdac067c6ac9d222c85d17790f25bc3c6
Horacinis <horacinis@gmail.com>
2021-07-22 00:30:06 +1000

    ...

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,12 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+  // console.log(con.connections);
+  console.log('DB connection successful!');
+});
 
 // prints development
-console.log(app.get('env')); 
+console.log(app.get('env'));
 console.log(process.env.NODE_ENV)
 // prints a banch of different variables
 // console.log(process.env);
