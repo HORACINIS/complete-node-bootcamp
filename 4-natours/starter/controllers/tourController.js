cad1a5a71d9d82bd1d9fb3469aaf72175907c780
Horacinis <horacinis@gmail.com>
2021-08-09 21:48:13 +1000

    ...

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -16,10 +16,30 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // BUILD QUERY 
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    console.log(req.query, queryObj);
 
-    console.log(req.query);
+    const query = Tour.find(queryObj);
 
-    const tours = await Tour.find();
+    // BOTH METHODS BELOW DO THE EXACT SAME THING
+    // const query = Tour.find({
+    //   duration: '5',
+    //   difficulty: 'easy'
+    // });
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)   // ---> here an alternative can be  .lte(5) or .lte(5)
+    //   .where('difficulty')
+    //   .equals('easy')
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res
       .status(200)
       .json({
