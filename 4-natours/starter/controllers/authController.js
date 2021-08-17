ac587516d916df7408f6f356fbc9bae0a3dddc4b
Horacinis <horacinis@gmail.com>
2021-08-17 23:58:09 +1000

    ...

---
+++ 4-natours/starter/controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('./../models/usersModel');
+// const catchAsync = require('./../utils/catchAsync');
+
+exports.signup = async (req, res, next) => {
+
+  try {
+    const newUser = await User.create(req.body);
+    res
+      .status(201)
+      .json({
+        status: 'success',
+        data: {
+          user: newUser
+        }
+      });
+  } catch (err) {
+    res
+      .status(400)
+      .json({
+        status: 'fail',
+        message: err
+      });
+  }
+}
